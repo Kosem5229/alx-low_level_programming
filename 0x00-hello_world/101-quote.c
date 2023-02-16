01f33c9dff007680f14ea1fa308deef05b83735a
edna <ednaasaka55@gmail.com>
2023-02-16 14:18:01 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,9 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
-* main - Entry point
-*
-* Return: Always 1 (Error code)
-*/
+ * main - Entry
+ * Return: 1
+ */
 int main(void)
-{
-const char *msg1 = "and that piece of art is useful\" - ";
-const char *msg2 = "Dora Korpar, 2015-10-19\n";
-write(2, msg1, 36);
-write(2, msg2, 30);
-return (1);
-}
+	write(1, 'and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1)
