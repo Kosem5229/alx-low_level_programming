0619fc46324c8dcebb4d482b82d75be572865b4e
edna <ednaasaka55@gmail.com>
2023-02-22 07:42:49 -0800

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* times_table - prints the 9 times table, starting with 0.
+*
+* Description: This function prints the multiplication table for the number 9
+*              from 0 to 9, formatted in a 3-digit field.
+* Return: void
+*/
+void times_table(void)
+{
+int i, j, product;
+
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+product = i * j;
+if (j == 0)
+{
+printf("%d", product);
+}
+else if (product < 10)
+{
+printf("   %d", product);
+}
+else if (product >= 10)
+{
+printf("  %d", product);
+}
+}
+printf("\n");
+}
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,23 +1,9 @@
-int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
 
-void print_alphabet(void);
+#include <stdio.h>
 
-void print_alphabet_x10(void);
+void times_table(void);
 
-int _islower(int c);
+#endif /* MAIN_H */
 
-int _isalpha(int c);
-
-int print_sign(int n);
-
-int _abs(int);
-
-int print_last_digit(int);
-
-void jack_bauer(void);
-
-int add(int, int);
-
-void print_to_98(int n);
-
-void print_times_table(int n);
