c5008060a468058bd85eef2eef9a970f30e246cb
edna <ednaasaka55@gmail.com>
2023-02-22 02:23:22 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,13 @@
 */
 int print_last_digit(int n)
 {
+long num = n;
 int last_digit;
 
-if (n < 0)
-n = -n;
+if (num < 0)
+num = -num;
 
-last_digit = n % 10;
+last_digit = num % 10;
 _putchar(last_digit + '0');
 
 return (last_digit);
