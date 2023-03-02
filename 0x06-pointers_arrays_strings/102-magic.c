454e6cbcc179cc78b5d85783ce9a0b52aca6c5b5
edna <ednaasaka55@gmail.com>
2023-03-02 12:04:35 -0800

    task 10

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int n;
+int a[5];
+int *p;
+a[2] = 1024;
+p = &n;
+*(p = &a[2] - 1) = 98;
+printf("a[2] = %d\n", a[2]);
+return (0);
+}
+
